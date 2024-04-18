fcda29c94917fdc9d8f91c863325491b6eaf53fd
harin95 <76214729+harin95@users.noreply.github.com>
2024-04-18 23:48:19 +0900

    Title: 코드트리 투어, Time: 5054ms, Memory: 40MB, Status: Time Limit Exceed - CodeTree

--- 240418/코드트리 투어/codetree-tour.py
+++ 240418/코드트리 투어/codetree-tour.py
@@ -1,9 +1,7 @@
 import heapq
 from collections import defaultdict
-# import sys
-# sys.stdin = open("input.txt", "r")
 
-
+# 최소 거리 구하기
 def run_dijkstra(start):
     distance = [float("inf")] * n
     distance[start] = 0
@@ -44,38 +42,45 @@ for i in range(0, (m-1)*3+1, 3):
     edges[fr].append([cost, to])
     edges[to].append([cost, fr])
 
-# init
+# 초기화
 distance = run_dijkstra(0)
 id_tf = [False] * 30001
 
+# 여행상품 저장
 product = []
 
 for _ in range(Q-1):
     cmd = input().split()
-    if cmd[0] == '200':
+    if cmd[0] == '200':     # 여행 상품 생성
         id, rev, dest = list(map(int, cmd[1:]))
-        inf_flag = -1 if distance[dest] == float("inf") else 0
-        product.append([id, rev, dest, inf_flag])
+        flag = -1 if distance[dest] == float("inf") else 0
+        product.append([id, rev, dest, flag])
         id_tf[id] = True
-    elif cmd[0] == '300':
+
+    elif cmd[0] == '300':    # 여행 상품이 존재할 경우 취소
         id = int(cmd[1])
-        id_tf[id] = False
+        if id_tf[id]:
+            id_tf[id] = False
+
     elif cmd[0] == '400':
         if not product:
             print(-1)
         else:
-            product.sort(key=lambda x: (inf_flag, x[1]-distance[x[2]], -x[0]))   # 수익 오름차순, 아이디 내림차순
-            # 도달하지 못하는 경우
-            if product[-1][3] == -1:
-                print(-1)
+            product.sort(key=lambda x: (x[3], x[1]-distance[x[2]], -x[0]))   # 수익 오름차순, 아이디 내림차순 (맨 뒤에서 뽑기 위해서 반대로)
+
             # 삭제된 경우
-            elif not id_tf[product[-1][0]]:
+            while product and not id_tf[product[-1][0]]:
                 product.pop()
+            if not product:
+                print(-1)
+
+            # 도달하지 못하는 경우
+            elif product[-1][3] == -1:
                 print(-1)
             # 이득이 없는 경우
             elif product[-1][1] - distance[product[-1][2]] < 0:
                 print(-1)
-            else:
+            else:   # 판매 가능
                 res = product.pop()
                 print(res[0])
 
@@ -83,4 +88,4 @@ for _ in range(Q-1):
         start = int(cmd[1])
         distance = run_dijkstra(start)
         for prod in product:
-            prod[3] = -1 if distance[prod[2]] == float("inf") else 0
+            prod[3] = -1 if distance[prod[2]] == float("inf") else 0    # 시작점 바뀌면 도달하지 못하는 경우 다시 계산
